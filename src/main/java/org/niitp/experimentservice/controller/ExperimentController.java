031a24ff12f68d480fc9b5bf0a59520a8c65dbed
snaiperlfc <snaiperlfc@ya.ru>
2024-11-28 05:12:36 +0300

    fix

--- src/main/java/org/niitp/experimentservice/controller/ExperimentController.java
+++ src/main/java/org/niitp/experimentservice/controller/ExperimentController.java
@@ -1,5 +1,7 @@
 package org.niitp.experimentservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.niitp.experimentservice.model.Experiment;
 import org.niitp.experimentservice.service.ExperimentService;
@@ -13,6 +15,7 @@ import reactor.core.publisher.Mono;
 @RestController
 @Slf4j
 @RequestMapping("/experiments")
+@Tag(name = "info", description = "Предоставление информации об эксперименте")
 public class ExperimentController {
 
     final
@@ -22,11 +25,17 @@ public class ExperimentController {
         this.experimentService = experimentService;
     }
 
+    @Operation(
+            summary = "Получить данные",
+            description = "Получаем данные")
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Experiment> index() {
         return experimentService.getExperiments();
     }
 
+    @Operation(
+            summary = "Добавить данные",
+            description = "Добавляем данные")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<Experiment> addExperiment(@RequestBody Experiment experiment) {
@@ -34,6 +43,9 @@ public class ExperimentController {
         return experimentService.addExperiment(experiment);
     }
 
+    @Operation(
+            summary = "Получить данные",
+            description = "Получаем данные, указав id")
     @GetMapping(value = "/{id}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Experiment> getExperimentById(@PathVariable String id) {
         return experimentService.getExperimentById(id);
